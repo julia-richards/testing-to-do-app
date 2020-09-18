16ef9c6ee058d9331b13b711834baec660ea5c56
julia-richards <julia.m.richards@gmail.com>
2020-09-18 15:10:33 -0600

    compelted merge-items tests

--- test/merge-items-spec.js
+++ test/merge-items-spec.js
@@ -1,39 +1,87 @@
-const { expect } = require('chai');
-const { mergeItems } = require('../merge-items');
+const { expect } = require("chai");
+const { mergeItems } = require("../merge-items");
 describe("The mergeItems function", () => {
-  const template = `
+	const template = `
     <table>
       <tbody>
         <!-- Content here -->
       </tbody>
     </table>
   `;
-  it("should return no <tr>s and no <td>s for no items", () => {
-    const items = [];
+	it("should return no <tr>s and no <td>s for no items", () => {
+		const items = [];
 
-    const result = mergeItems(template, items)
+		const result = mergeItems(template, items);
+
+		expect(result).to.contain("<table>");
+		expect(result).to.contain("</table>");
+		expect(result).to.contain("<tbody>");
+		expect(result).to.contain("</tbody>");
+		expect(result).to.not.contain("<tr>");
+		expect(result).to.not.contain("</tr>");
+		expect(result).to.not.contain("<td>");
+		expect(result).to.not.contain("</td>");
+		expect(result).to.not.contain("<!-- Content here -->");
+	});
+
+	it("should return a single <tr>, four <td>s, and a <form> for one uncompleted item", () => {
+		const items = [{ title: "Title 1", category: "Category 1" }];
+		const result = mergeItems(template, items);
+
+		expect(result).to.contain("<table>");
+		expect(result).to.contain("</table>");
+		expect(result).to.contain("<tbody>");
+		expect(result).to.contain("</tbody>");
+		expect(result).to.contain("<tr>");
+		expect(result).to.contain("</tr>");
+		expect(result).to.contain("<td>Title 1</td>");
+		expect(result).to.contain("<td>Category 1</td>");
+		expect(result).to.contain('<form method="POST" action="/items/1">');
+
+		expect(result).to.not.contain("<!-- Content here -->");
+	});
+
+	it("should return a single <tr>, four <td>s, and no <form> for one completed item", () => {
+		const items = [
+      { title: 'Title 1', category: 'Category 1', isComplete: true },
+    ];
+		const result = mergeItems(template, items);
+
+		expect(result).to.contain("<table>");
+		expect(result).to.contain("</table>");
+		expect(result).to.contain("<tbody>");
+		expect(result).to.contain("</tbody>");
+		expect(result).to.contain("<tr>");
+		expect(result).to.contain("</tr>");
+		expect(result).to.contain("<td>Title 1</td>");
+		expect(result).to.contain("<td>Category 1</td>");
+
+		expect(result).to.not.contain('<form method="POST" action="/items/1">');
+		expect(result).to.not.contain("<!-- Content here -->");
+	});
+
+	it("should return three <tr>s for three items", () => {
+    const items = [
+      { title: 'Title 1', category: 'Category 1'}, {title: "Title 2", category: "Category 2"}, {title: "Title 3", category: "Category 3"}
+    ];
+
+    const result = mergeItems(template, items);
 
     expect(result).to.contain("<table>");
-    expect(result).to.contain("</table>");
-    expect(result).to.contain("<tbody>");
-    expect(result).to.contain("</tbody>");
-    expect(result).to.not.contain("<tr>");
-    expect(result).to.not.contain("</tr>");
-    expect(result).to.not.contain("<td>");
-    expect(result).to.not.contain("</td>");
-    expect(result).to.not.contain("<!-- Content here -->");
+		expect(result).to.contain("</table>");
+		expect(result).to.contain("<tbody>");
+		expect(result).to.contain("</tbody>");
+		expect(result).to.contain("<tr>");
+		expect(result).to.contain("</tr>");
+		expect(result).to.contain("<td>Title 1</td>");
+    expect(result).to.contain("<td>Category 1</td>");
+    expect(result).to.contain("<td>Title 2</td>");
+    expect(result).to.contain("<td>Category 2</td>");
+    expect(result).to.contain("<td>Title 3</td>");
+		expect(result).to.contain("<td>Category 3</td>");
 
-  });
+		expect(result).to.contain('<form method="POST" action="/items/1">');
+		expect(result).to.not.contain("<!-- Content here -->");
 
-  it("should return a single <tr>, four <td>s, and a <form> for one uncompleted item", () => {
-    expect.fail('please write this test');
-  });
-
-  it("should return a single <tr>, four <td>s, and no <form> for one completed item", () => {
-    expect.fail('please write this test');
-  });
-
-  it("should return three <tr>s for three items", () => {
-    expect.fail('please write this test');
-  });
+	});
 });
