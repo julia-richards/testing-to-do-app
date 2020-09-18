687bf8ae16a6eeea27f58aa4f513a978461570ac
julia-richards <julia.m.richards@gmail.com>
2020-09-18 13:51:18 -0600

    finished tests for create and server category, began working on save sumbitted category tests

--- test/merge-categories-spec.js
+++ test/merge-categories-spec.js
@@ -1,9 +1,9 @@
-const { expect } = require('chai');
-const { mergeCategories } = require('../merge-categories');
+const { expect } = require("chai");
+const { mergeCategories } = require("../merge-categories");
 
 describe("mergeCategories()", () => {
-  context("Using <li> tags", () => {
-    const template = `
+	context("Using <li> tags", () => {
+		const template = `
       <div>
         <ul>
           <!-- Content here -->
@@ -11,20 +11,53 @@ describe("mergeCategories()", () => {
       </div>
     `;
 
-    it("should return no <li>s for no categories", () => {
-    });
+		it("should return no <li>s for no categories", () => {
+			//Arrange
+			const categories = [];
+			//Act
+			const result = mergeCategories(template, categories, "li");
+			//Assert
+			expect(result).to.contain("<div>");
+			expect(result).to.contain("</div>");
+			expect(result).to.contain("<ul>");
+			expect(result).to.contain("</ul>");
+			expect(result).to.not.contain("<li>");
+			expect(result).to.not.contain("</li>");
+		});
 
-    it("should return a single <li> for one category", () => {
-      expect.fail('please write this test');
-    });
+		it("should return a single <li> for one category", () => {
+			//Arrange
+			const categories1 = ["str"];
+			//Act
+			const result = mergeCategories(template, categories1, "li");
+			//Assert
+			expect(result).to.contain("<div>");
+			expect(result).to.contain("</div>");
+			expect(result).to.contain("<ul>");
+			expect(result).to.contain("</ul>");
+			expect(result).to.contain("<li>str</li>");
+			expect(result).to.not.contain("<!-- Content here -->");
+		});
 
-    it("should return an <li> for each category", () => {
-      expect.fail('please write this test');
-    });
-  });
+		it("should return an <li> for each category", () => {
+			//Arrange
+			const categories = ["first str", "second str", "third str"];
+			//Act
+			const result = mergeCategories(template, categories, "li");
+			//Assert
+			expect(result).to.contain("<div>");
+			expect(result).to.contain("</div>");
+			expect(result).to.contain("<ul>");
+			expect(result).to.contain("</ul>");
+			expect(result).to.contain("<li>first str</li>");
+			expect(result).to.contain("<li>second str</li>");
+			expect(result).to.contain("<li>third str</li>");
+			expect(result).to.not.contain("<!-- Content here -->");
+		});
+	});
 
-  context("using <option> tags", () => {
-    const template = `
+	context("using <option> tags", () => {
+		const template = `
       <div>
         <select>
           <!-- Content here -->
@@ -32,16 +65,16 @@ describe("mergeCategories()", () => {
       </div>
     `;
 
-    it("should return no <option>s for no categories", () => {
-      expect.fail('please write this test');
-    });
+		it("should return no <option>s for no categories", () => {
+			expect.fail("please write this test");
+		});
 
-    it("should return a single <option> for one category", () => {
-      expect.fail('please write this test');
-    });
+		it("should return a single <option> for one category", () => {
+			expect.fail("please write this test");
+		});
 
-    it("should return an <option> for each category", () => {
-      expect.fail('please write this test');
-    });
-  });
+		it("should return an <option> for each category", () => {
+			expect.fail("please write this test");
+		});
+	});
 });
